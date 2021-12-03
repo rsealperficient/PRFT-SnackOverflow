03d1ae75d44db3538458a656a694c700d64d6e5b
populationtwo <populationtwo@gmail.com>
2021-12-03 16:27:14 -0500

    Downgrade strapi to version 3

--- backend/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '38649dacbce07318c628afdf15c84df4'),
-  },
-});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cd05632b-e0ff-412b-aa85-0c2bbbf9db15'
+};
