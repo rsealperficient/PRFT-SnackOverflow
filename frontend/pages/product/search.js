1482893450e9bed043afb35c6874a76336e87bcf
populationtwo <populationtwo@gmail.com>
2021-12-05 18:37:33 -0500

    Fix Search Page layout

--- frontend/pages/product/search.js
+++ frontend/pages/product/search.js
@@ -18,6 +18,8 @@ import {
   useColorModeValue,
   Center,
   Spinner,
+  useBreakpointValue,
+  Stack,
 } from "@chakra-ui/react"
 import Image from "next/image"
 import PageLayout from "@/components/page-layout"
@@ -28,6 +30,7 @@ import { useRouter } from "next/router"
 
 import { API_URL } from "@/config/index"
 import React from "react"
+import { MdKeyboardArrowLeft } from "react-icons/md"
 
 function ProductSearchPage({ products }) {
   const router = useRouter()
@@ -40,11 +43,42 @@ function ProductSearchPage({ products }) {
   }
   return (
     <PageLayout>
-      <Container maxW={"container.xl"}>
-        <Heading>Search Results for {router.query.term}</Heading>
-        {products.length === 0 && <h3>No products to show</h3>}
+      <Flex
+        w={"full"}
+        h={"20vh"}
+        bgGradient="linear(to-r, teal.300, cyan.700, purple.500)"
+        backgroundSize={"cover"}
+        backgroundPosition={"center center"}
+      >
+        <VStack
+          w={"full"}
+          justify={"center"}
+          px={useBreakpointValue({ base: 4, md: 8 })}
+          bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+        >
+          <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
+            <Heading
+              color={"white"}
+              fontWeight={700}
+              lineHeight={1.2}
+              fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
+            >
+              Search Results for {router.query.term}
+            </Heading>
+          </Stack>
+        </VStack>
+      </Flex>
+      <Container
+        maxW={"container.xl"}
+        pt={"8"}
+        pb={"20"}
+        minH={"calc(100vh - 88px - 117px - 56px )"}
+      >
+        <Button onClick={() => router.back()} variant={"link"} size={"small"}>
+          <MdKeyboardArrowLeft /> Back
+        </Button>
+        {products.length === 0 && <Heading>No products to show</Heading>}
         <ProductsList products={products} />
-        <Button onClick={() => router.back()}>Go back</Button>
       </Container>
     </PageLayout>
   )
@@ -53,7 +87,6 @@ function ProductSearchPage({ products }) {
 export default ProductSearchPage
 
 export async function getServerSideProps({ query: { term } }) {
-
   const query = qs.stringify({
     _where: {
       _or: [
